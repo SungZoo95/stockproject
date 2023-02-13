885964258259e2b5a4ac66834428983f6a1ae07a
SungZoo95 <astar1015@gmail.com>
2023-02-14 02:01:55 +0900

    0214make

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def dataframe1():
 
 def stock_date_input():
     df_1 = pd.read_csv('name_code_0206.csv', dtype=str)
-    df_2 = pd.read_csv('naver60pages.csv')
+    df_2 = pd.read_csv('naver_up_down_end.csv')
     df_2.drop(['Unnamed: 0'],axis=1,inplace=True)
     
     da = st.date_input(
@@ -97,7 +97,118 @@ def download(file):
         data=csv_data,
         file_name='name_code_0206.csv',
         mime='text/csv',)
+    
+def token():
+    df_1 = pd.read_csv("naver_up_down_end.csv").drop(["Unnamed: 0"],axis=1)
+    df_1[["날짜","기업명","제목+내용","전날_대비_상승_하락"]]
+    df_1.rename(columns={'전날_대비_상승_하락':'상승_하락'},inplace=True)
+    st.markdown('------')
+    st.header('댓글 정제화 작업')
+    st.markdown('- 온전한 한글외에 모든 부분을 제거했다')
+    st.markdown('- 초성,알파벳,특수문자,이모티콘 등을 제거했다')
+    code = '''df_1['정제된 댓글'] = df_1['제목+내용'].str.replace('\\[삭제된 게시물의 답글\\]',' ')
+                                    # 삭제된 게시물이 크롤링 된 경우가 있어 넣어주었다
+                    df_1['정제된 댓글'] = df_1['정제된 댓글'].str.replace('제목+내용:',' ')
+                    df_1['정제된 댓글'] = df_1['정제된 댓글'].str.replace('[^가-힣]',' ').str.replace(' +',' ').str.strip()
+                    # 한글만 나오게하는 정규식을 사용했고 \r같은 특수문자들이 포함되어 나와 제거해주었다
+                    df_1 = df_1[df_1['정제된 댓글'] != '']
+                    # 내용이 없는 댓글을 제거해주었다
+                    df_1 = df_1.reset_index(drop=True)
+                    df_1[["날짜","기업명","정제된 댓글","상승_하락"]]'''
+    st.code(code,language='python')
+    st.markdown('작업 후')
+    if st.button('정제화'):
+        df_1['정제된 댓글'] = df_1['제목+내용'].str.replace('\\[삭제된 게시물의 답글\\]',' ')
+        df_1['정제된 댓글'] = df_1['정제된 댓글'].str.replace('제목+내용:',' ')
+        df_1['정제된 댓글'] = df_1['정제된 댓글'].str.replace('[^가-힣]',' ').str.replace(' +',' ').str.strip()
+        df_1 = df_1[df_1['정제된 댓글'] != '']
+        df_1 = df_1.reset_index(drop=True)
+        df_1[["날짜","기업명","정제된 댓글","상승_하락"]]
+        st.write('실행 완료')
+    else : 
+        st.write('대기 중')
+    
+def token2():
+    df_1 = pd.read_csv('name_code_0206.csv')
+    key = 'shineekey'
+    option = st.selectbox("기업을 선택하세요", (df_1["종목명"]),key=key)
+    if st.button('토큰화'):
+        df_2 = pd.read_csv("konlpydata.csv").drop(["Unnamed: 0.1"],axis=1)
+        df_2.rename(columns={'전날_대비_상승_하락':'상승_하락'},inplace=True)
+        df_2 = df_2[df_2["기업명"]==option]
+        df_2[["날짜", "기업명","토큰화 댓글","상승_하락","label"]]
+        st.write('실행 완료')
 
+    else : 
+        st.write('대기 중')
+
+def konlpy():
+    st.markdown('- 정제화를 완료했다면 konlpy를 이용해 댓글들을 토크나이징 해준다')
+    code = '''def corpus_save(company):
+    df = clean_sents_df(company) #댓글 정제한 함수입니다
+    df['정제된 댓글 길이'] = [len(str(i)) for i in df['정제된 댓글']]
+    
+    # vocab.txt = 자연어 처리 사전입니다
+    tp = [str(i) for i in list(df['정제된 댓글'])]
+    save = '\ n'.join(tp)
+    f = open("vocab.txt", 'a',encoding='utf8')
+    f.write(save)
+    f.close()
+    
+def corpus_init():
+    f = open("vocab.txt", 'w',encoding='utf8')
+    f.write('')
+    f.close()
+    for company in company_list:
+        corpus_save(company)
+
+def return_tokenizer():
+    corpus = DoublespaceLineCorpus("vocab.txt",iter_sent=True)
+    noun_extractor = LRNounExtractor_v2(verbose=True)
+    nouns = noun_extractor.train_extract(corpus)
+    scores = {word:score.score for word, score in nouns.items()}
+    tokenizer = LTokenizer(scores=scores)
+    return tokenizer'''
+    st.code(code,language='python')
+    
+    st.markdown('----')
+    
+    st.markdown('''- 미리 준비한 긍정적 자연어 단어집과 부정적 단어집을 사용하여 
+                댓글들에 대한 라벨링 마킹합니다''')
+    code_1 = '''def labeling(target_df):
+    f = open("/neg_pol_word.txt", 'r',encoding='utf8')
+    words = f.readlines()
+    f.close()
+    neg_words_set = {word.strip('\n') for word in words}
+    # 부정적 단어집 열기
+    
+    f = open("pos_pol_word.txt", 'r',encoding='utf8')
+    words = f.readlines()
+    f.close()
+    pos_words_set = {word.strip('\n') for word in words}
+    
+    # 긍정적 단어집 열기
+    label_score = []
+    for token_list in target_df['토큰화 댓글']:
+        sent_score = 0
+        for token in token_list:
+            if token in neg_words_set:
+                sent_score -= 1
+            elif token in pos_words_set:
+                sent_score += 1
+    # 댓글의 단어들을 단어집에서 찾아 부정적인 단어라면 -1 긍정적 단어라면 +1점을 매깁니다
+   
+        if sent_score < 0:
+            label_score.append(-1)
+        elif sent_score > 0:
+            label_score.append(1)
+        else:
+            label_score.append(0)
+    # 댓글의 단어들로 매긴 점수로 라벨링을 매깁니다
+    
+    target_df['label'] = label_score
+    return target_df'''
+    st.code(code_1,language='python')
 #def image():
 #    
 #    image = Image.open('heechan.jpg')
@@ -128,18 +239,33 @@ def main():
     st.markdown('------') 
     
     st.header('날짜/기업별 댓글 조회')
+    st.markdown("네이버 종목토론실의 인기상위 30종목의 댓글을 수집했다")
+    st.markdown("날짜/기업 조합으로 댓글 확인이 가능하다")
     stock_date_input()
     st.markdown('------')
 
-    st.header('기업정보')
+    st.header('기업 주식정보')
+    st.markdown("기업의 주식정보를 추가로 수집해 원하는 정보별로 확인가능하다")
     download('top30stock.csv')
     st.markdown('삼성 주식정보 다운로드')
-    if st.checkbox("기업정보"):
+    if st.checkbox("기업 주식정보"):
         dataframe2()
+        
     dataframe2_add()   
     st.markdown('-----')
     makegraph()
-   
+    st.markdown('-----')
+
+    st.header('전처리를 위한 댓글 확인')
+    st.markdown('''댓글을 확인하면 초성,알파벳,특수문자 등등 정제가 필요한 정보들이 
+                있음을 확인 할 수 있다''')    
+    token()
+    st.markdown('정제화가 된 댓글들을 확인 할 수 있다')
+    st.markdown('------')
+    st.header('토큰화 작업')
+    konlpy()
+    token2()
+    st.markdown('토큰화 작업과 라벨링작업까지 완료')
     
 if __name__ == "__main__":
     main()

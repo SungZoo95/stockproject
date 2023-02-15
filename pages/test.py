2799d4b73d7e4b0c6ea3bc336bd87c610f5d09bf
SungZoo95 <astar1015@gmail.com>
2023-02-15 09:22:14 +0900

    data

--- pages/test.py
+++ pages/test.py
@@ -4,7 +4,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import streamlit as st
 
 import soynlp
 from soynlp.noun import LRNounExtractor_v2
@@ -17,15 +17,15 @@ warnings.filterwarnings('ignore')
 
 # 1. 데이터 로드 및 확인 
 target = pd.read_csv('filter.csv', encoding = 'UTF-8').drop(['Unnamed: 0'], axis=1)
-#target.columns
-#target.shape 
+target.columns
+target.shape 
 
 # train,test 분리 
 d_train = target.sample(frac=0.70, random_state=2023)
-#d_train.shape
-#d_train
+d_train.shape
+d_train
 d_test = target.drop(d_train.index)
-#d_test.shape
+d_test.shape
 
 # 2. 데이터 전처리 
 def clean_sents_df(company):
@@ -37,6 +37,7 @@ def clean_sents_df(company):
     df = df[df['정제된 내용'] != '']
     df = df.reset_index(drop=True)
     return  df
+
 def clean_test_df(company):
     target_company = d_test[d_test['기업명'] == company]
     df = target_company
@@ -50,6 +51,13 @@ def clean_test_df(company):
 
 
 
+st.title("테스트")
 
 clean_sents_df('포스코케미칼')
 clean_sents_df('삼성전자')
+
+company_list = list(d_train["기업명"])
+company_list = list(set(company_list))
+company_list
+
+len(company_list)
